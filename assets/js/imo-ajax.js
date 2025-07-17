506835ed05bd4dc365d6b9a791fc1455fe72d0ef
Xolof <oljo@protonmail.ch>
2025-07-17 07:53:20 +0200

    Add a script for calling the convert endpoint and showing data about the conversion job.

---
+++ assets/js/imo-ajax.js
@@ -0,0 +1,84 @@
+const nonce = imageOptimizer.nonce;
+const pluginsUrl = imageOptimizer.pluginsUrl;
+const convertButton = document.getElementById('convert-button');
+const imagesToBeConvertedInfo = document.querySelector(".imagesToBeConvertedInfo");
+const loader = document.querySelector(".loader");
+
+async function fetchPrivateData() {
+    const resultsDiv = document.querySelector('.resultsDiv');
+    const logDiv = document.querySelector('.logDiv');
+
+    logDiv.innerHTML = "";
+    convertButton.style.display = "none";
+    imagesToBeConvertedInfo.style.display = "none";
+
+    try {
+        resultsDiv.innerHTML = "";
+        resultsDiv.classList.add("inProgress");
+        resultsDiv.textContent = "Converting...";
+        loader.style.display = "block";
+
+        const interval = setInterval(showLogData, 50);
+
+        const response = await fetch('/wp-json/imo/v1/convert', {
+            method: 'GET',
+            headers: {
+                'X-WP-Nonce': nonce,
+            },
+            credentials: 'same-origin',
+        });
+
+        if (!response.ok) {
+            throw new Error('Request failed with status ' + response.status);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        loader.style.display = "none";
+        resultsDiv.innerHTML = "";
+        resultsDiv.textContent = data.message;
+        resultsDiv.classList.add("done");
+        clearInterval(interval);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+async function showLogData() {
+    const logDiv = document.querySelector('.logDiv');
+
+    const url = pluginsUrl + "/image_optimizer/imo.log";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        let text = await response.text();
+        logDiv.style.display = "block";
+        logDiv.innerHTML = "";
+        text = text.replace(/&quot;/gi, '')
+            .replace(/\\\//gi, '/')
+            .replace(/\n/gi, "<br>");
+
+        text = text.replace(
+            /(Error.+details.)/,
+            `<div style='
+                background: #ff8484ff;
+                color: #000;
+                padding: 0.4em;
+                border-radius: 4px;
+            '>$1</div>
+            `);
+
+        logDiv.innerHTML = text
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+
+if (convertButton) {
+    convertButton.addEventListener('click', fetchPrivateData);
+}
