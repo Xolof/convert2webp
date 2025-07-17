1714ee5d8538a7c1775edbef16f8eb44adad60ba
Xolof <oljo@protonmail.ch>
2025-07-17 08:00:17 +0200

    Update the template for the conversion page
    We remove the form and use a button to make an AJAX request.

--- templates/settings.php
+++ templates/settings.php
@@ -12,44 +12,32 @@ if (! defined('ABSPATH')) {
 
 ?>
 
-<h1><?php echo esc_html__('Image Optimizer', 'imo'); ?></h1>
+<main class="imoMain">
+    <h1><?php echo esc_html__('Image Optimizer', 'imo'); ?></h1>
 
-<?php if (current_user_can('manage_options')) : ?>
-    <?php $imo_nonce = wp_create_nonce('imo_nonce'); ?>
+    <?php if (current_user_can('manage_options')) : ?>
+        <?php $imo_nonce = wp_create_nonce('imo_nonce'); ?>
 
-    <?php $imageCount = count($images) ?>
-    <p>Found <?= $imageCount ?> Jpg and Png images which can be converted to Webp.</p>
+        <?php $imageCount = count($images) ?>
 
+        <?php if ($imageCount) : ?>
+            <p class="imagesToBeConvertedInfo">
+                Found <?= $imageCount ?> Jpg and Png images which can be converted to Webp.
+            </p>
+            <button id="convert-button" class="button button-primary">Convert</button>
+            <div class="resultsDiv">
+                <h3>Images to be converted</h3>
+                <?php foreach ($images as $image) : ?>
+                    <p><?= esc_html($image) ?></p>
+                <?php endforeach; ?>
+            </div>
+            <span class="loader"></span>
+            <div class="logDiv"></div>
+        <?php else : ?>
+            <p class="imagesToBeConvertedInfo">There are no Jpg or Png images to be converted.</p>
+        <?php endif; ?>
 
-    <!--
-        TODO
-        Make an AJAX endpoint for converting.
-        Make an AJAX endpoint for showing the log.
-        Print the log in real time.
-    -->
-    <?php if ($imageCount) : ?>
-        <div class="imo_form">
-            <form
-                action="<?php echo esc_url(admin_url('admin-post.php')); ?>"
-                method="POST"
-                id="imo_form">
-                <input type="hidden" name="action" value="imo_form_response" />
-                <input type="hidden" name="imo_nonce" value="<?php echo esc_html($imo_nonce); ?>" />
-                <input
-                    type="submit"
-                    name="submit"
-                    id="submit"
-                    class="button button-primary"
-                    value="<?php echo esc_html__('Convert to Webp', 'imo'); ?>"
-                />
-            </form>
-        </div>
+    <?php else : ?>
+        <p><?php echo esc_html__('You are not authorized to perform this operation.', 'imo'); ?></p>
     <?php endif; ?>
-
-    <?php foreach ($images as $image) : ?>
-        <p><?= esc_html($image) ?></p>
-    <?php endforeach; ?>
-
-<?php else : ?>
-    <p><?php echo esc_html__('You are not authorized to perform this operation.', 'imo'); ?></p>
-<?php endif; ?>
+</main>
